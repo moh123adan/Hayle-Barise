5519ff67966e0e0d0c1bc559e2680f682855535f
moh123adan <maxamedadan169@gmail.com>
2025-01-07 18:36:08 +0300

    added: latest news component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/hero";
 import SafetyMeasures from "./components/safety-measures";
 import FeaturesGrid from "./components/features-grid";
+import LatestNews from "./components/latest-news";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <SafetyMeasures />
       <FeaturesGrid /> 
+      <LatestNews />
     </main>
   );
 }

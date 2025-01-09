e55e9a8df8aaf75762c1b89606c25a412fe8451e
moh123adan <maxamedadan169@gmail.com>
2025-01-09 19:16:04 +0300

    added: map

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -54,12 +54,14 @@ export default function ContactUs() {
 
           {/* Map */}
           <div className="relative h-[400px]">
-            <Image
-              src="/placeholder.svg?height=400&width=600"
-              alt="School location map"
-              fill
-              className="object-cover"
-            />
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.0863874611636!2d-122.40082868468193!3d37.78928077975665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858064df00b755%3A0xa7d5f01e6f544c8e!2s500%20Terry%20Francine%20St%2C%20San%20Francisco%2C%20CA%2094158%2C%20USA!5e0!3m2!1sen!2sin!4v1673016081901!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              allowFullScreen=""
+              loading="lazy"
+              className="border-0"
+            ></iframe>
           </div>
 
           {/* Contact Form */}

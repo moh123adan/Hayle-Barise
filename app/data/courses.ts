75468591fa4fc76a59b8514c29883ac607aa6854
moh123adan <maxamedadan169@gmail.com>
2025-02-01 13:34:35 +0300

    added: fixed images path

--- app/data/courses.ts
+++ app/data/courses.ts
@@ -1,10 +1,10 @@
-import type { Course } from "@/types/course"
+import type { Course } from "../types/course"
 
 export const courses: Course[] = [
     {
         id: 1,
         title: "Machanical Engineering",
-        image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
+        image: "/images/machanical.png",
         startDate: "2024-03-01",
         duration: "12 months",
         studyMode: "Diploma",
@@ -13,7 +13,7 @@ export const courses: Course[] = [
     {
         id: 2,
         title: "Mason, Stone-Setting & Tiling",
-        image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
+        image: "/images/mason.png",
         startDate: "2024-05-01",
         duration: "2 Years",
         studyMode: "Diploma",
@@ -22,7 +22,7 @@ export const courses: Course[] = [
     {
         id: 3,
         title: "Electrical Installation & Solar PV",
-        image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
+        image: "/images/electerical1.png",
         startDate: "2020-01-26",
         duration: "2 Years",
         studyMode: "Diploma",
--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,19 @@ import EngineeringPrograms from "./components/engineering-programs";
 import IndustryStandards from "./components/industry-standards";
 import EngineeringHighlights from "./components/tech-highlits";
 import ServicesSection from "./components/service-section";
+import { CoursesSlider } from "./components/courses-slider";
+import { courses } from "./data/courses";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <IndustryStandards />
-      <FeaturesGrid /> 
+      <FeaturesGrid />
       <EngineeringHighlights />
       <EngineeringPrograms />
       <ServicesSection />
+      <CoursesSlider courses={courses} />
       <NewsletterForm />
       <Footer />
     </main>

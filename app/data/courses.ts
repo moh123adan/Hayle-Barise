115ef7e7128c4dd9d7cf025bef6a628972479e35
moh123adan <maxamedadan169@gmail.com>
2025-02-01 13:22:36 +0300

    added: course type

--- app/data/courses.ts
+++ app/data/courses.ts
@@ -1,32 +1,32 @@
 import type { Course } from "@/types/course"
 
 export const courses: Course[] = [
-  {
-    id: 1,
-    title: "Machanical Engineering",
-    image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
-    startDate: "2024-03-01",
-    duration: "12 months",
-    studyMode: "Diploma",
-    weeklyStudy: "2 hours",
-  },
-  {
-    id: 2,
-    title: "Mason, Stone-Setting & Tiling",
-    image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
-    startDate: "2024-05-01",
-    duration: "2 Years",
-    studyMode: "Diploma",
-    weeklyStudy: "25 hours",
-  },
-  {
-    id: 3,
-    title: "Electrical Installation & Solar PV",
-    image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
-    startDate: "2020-01-26",
-    duration: "2 Years",
-    studyMode: "Diploma",
-    weeklyStudy: "25 hours",
-  },
+    {
+        id: 1,
+        title: "Machanical Engineering",
+        image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
+        startDate: "2024-03-01",
+        duration: "12 months",
+        studyMode: "Diploma",
+        weeklyStudy: "2 hours",
+    },
+    {
+        id: 2,
+        title: "Mason, Stone-Setting & Tiling",
+        image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
+        startDate: "2024-05-01",
+        duration: "2 Years",
+        studyMode: "Diploma",
+        weeklyStudy: "25 hours",
+    },
+    {
+        id: 3,
+        title: "Electrical Installation & Solar PV",
+        image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-g0YHhvHmKn3CcAQ1iIHe8nPOuYW8t2.png",
+        startDate: "2020-01-26",
+        duration: "2 Years",
+        studyMode: "Diploma",
+        weeklyStudy: "25 hours",
+    },
 ]
 
---
+++ app/types/course.ts
@@ -0,0 +1,11 @@
+export interface Course {
+    id: number
+    title: string
+    image: string
+    startDate: string
+    duration: string
+    studyMode: string
+    weeklyStudy: string
+  }
+  
+  
